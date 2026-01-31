02d5b6a7d1142b1b1d8ce223d8f27e7186437fcd
bludzetelksina <bludzetelksina@gmail.com>
2026-01-31 10:05:57 +0300

    Simplify rendering of App component with HashRouter
    Removed unnecessary root container check and createRoot call.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container missing in index.html");
-}
-
-createRoot(container).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+<HashRouter>
+  <App />
+</HashRouter>
