2ec49535f5c65691582a82b6a6ea72379ab24594
bludzetelksina <bludzetelksina@gmail.com>
2026-01-31 10:52:49 +0300

    Refactor App component to use wouter with hash routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,32 @@
-import { Routes, Route } from "react-router-dom";
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      {/* Fallback to 404 */}
       <Route component={NotFound} />
     </Switch>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router />
+        <Router hook={useHashLocation}>
+          <AppRouter />
+        </Router>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
-
-export default App;
