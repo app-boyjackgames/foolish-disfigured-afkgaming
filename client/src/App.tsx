d325e8737d11b425e3b58f5a06a8b57c9d24c41e
bludzetelksina <bludzetelksina@gmail.com>
2026-01-31 10:46:04 +0300

    Switch to react-router-dom for routing in App
    Refactor App component to use react-router-dom for routing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -27,4 +28,10 @@ function App() {
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Главная</h1>} />
+    </Routes>
+  );
+}
